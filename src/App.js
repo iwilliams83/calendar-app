afcbb166c3c4df28e73d398dc1c2974befc80e73
Indira Williams <indirajamurath@gmail.com>
2019-01-04 10:25:56 -0500

    create month table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Month from './Month'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          calendar goes here
-        </header>
+        <Month />
       </div>
     );
   }
--- src/Month.js
+++ src/Month.js
@@ -1,1 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Icon, Table } from 'semantic-ui-react'
+
+class Month extends Component {
+  render(){
+    return   <div className="calendar-table">
+    <Table celled striped>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell colSpan='7' textAlign='center'>January</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+            <Table.Cell textAlign='center'>X</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </div>
+  }
+}
+
+
+
+export default Month
