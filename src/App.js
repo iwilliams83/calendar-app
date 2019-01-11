235b8be919ce18ef51001ab19497c0ccd99fe472
Indira Williams <indirajamurath@gmail.com>
2019-01-11 12:47:28 -0500

    buttons work, displays correct month

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import { months } from './months.json';
-import './App.css';
-import Month from './Month'
-import { Button } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { months } from "./months.json";
+import "./App.css";
+import Month from "./Month"
+import { Button } from "semantic-ui-react"
 
 
 
 class App extends Component {
+  state = {
+    monthToDisplay: 0
+  }
+
+  setDisplayMonth = (e, month) => {
+    this.setState({ monthToDisplay: month })
+  }
+
   render() {
     const colors = [
-      'olive', 'green', 'teal', 'blue', 'violet', 'purple',
-      'pink', 'brown', 'red', 'orange', 'yellow', 'olive'
+      "olive", "green", "teal", "blue", "violet", "purple",
+      "pink", "brown", "red", "orange", "yellow", "olive"
     ]
+
+    const m = this.state.monthToDisplay
+
     return (
       <div >
-        <div className='month-buttons'>
+        <div className="header"><h1>2019</h1></div>
+        <div className="month-buttons">
           {months.map((month, idx) => {
-            return <Button key={idx} color={colors[idx]}>
+            return <Button
+                    key={idx} color={colors[idx]} month={idx}
+                    onClick={(e, { month }) => this.setDisplayMonth(e, month)}
+                  >
                 {month.name}
               </Button>
         })}
         </div>
-        <div>
-          <Month month={months[5]}/>
+        <div className="month-display">
+          <Month month={months[m]}/>
         </div>
       </div>
     );
