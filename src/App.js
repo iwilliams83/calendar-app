87f232a9fa9d295bed1e9fdd5cf3e424f05a9261
Indira Williams <indirajamurath@gmail.com>
2019-01-11 11:58:06 -0500

    got buttons on the page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import { months } from './months.json';
 import './App.css';
 import Month from './Month'
+import { Button } from 'semantic-ui-react'
 
 
 
 class App extends Component {
   render() {
-
+    const colors = [
+      'olive', 'green', 'teal', 'blue', 'violet', 'purple',
+      'pink', 'brown', 'red', 'orange', 'yellow', 'olive'
+    ]
     return (
       <div >
-        <Month month={months[0]}/>
+        <div className='month-buttons'>
+          {months.map((month, idx) => {
+            return <Button key={idx} color={colors[idx]}>
+                {month.name}
+              </Button>
+        })}
+        </div>
+        <div>
+          <Month month={months[5]}/>
+        </div>
       </div>
     );
   }
